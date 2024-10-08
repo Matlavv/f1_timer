c90d5eb2e372a1640462d6dcde2226d795de2112
Matlavv <mathis.laversin92@gmail.com>
2024-10-08 11:47:01 +0200

    Initialize project

---
+++ backend/src/config/connection.ts
@@ -0,0 +1,14 @@
+// import mongoose from 'mongoose';
+
+// export const connect = async (): Promise<void> => {
+//   try {
+//     await mongoose.connect(MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error(error);
+//     console.log('MongoDB connection failed');
+//   }
+// };
