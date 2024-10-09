9773c5bdacbcbba97588638ee22ebbe4027ae927
Matlavv <mathis.laversin92@gmail.com>
2024-10-09 15:45:29 +0200

    Ajout du frontend

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -5,6 +5,8 @@ export interface IUser extends Document {
   email: string;
   password: string;
   role: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const UserSchema: Schema = new Schema({
@@ -12,6 +14,13 @@ const UserSchema: Schema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: Boolean, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+UserSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
 });
 
 export default mongoose.model<IUser>('User', UserSchema);
